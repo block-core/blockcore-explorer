131a2a57375382b0a17875ca17bae6ac6435706c
SondreB <sondre@outlook.com>
2021-02-18 14:39:40 +0100

    Add support for array in ticker API response

--- src/Blockcore.Explorer/ClientApp/src/app/ticker/ticker.component.ts
+++ src/Blockcore.Explorer/ClientApp/src/app/ticker/ticker.component.ts
@@ -25,7 +25,17 @@ export class TickerComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  navigator = (obj, path) => path.split('.').reduce((a, b) => a && a[b], obj);
+  navigator = (obj, path) => {
+    for (var i = 0, path = path.split('.'), len = path.length; i < len; i++) {
+      if (Array.isArray((obj[path[i]]))) {
+        obj = obj[path[i]][path[i + 1]];
+        i++;
+      } else {
+        obj = obj[path[i]];
+      }
+    };
+    return obj;
+  };
 
   async updateTicker() {
 
