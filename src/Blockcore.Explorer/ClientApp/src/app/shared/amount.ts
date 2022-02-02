5ae0ae1c46afd04f26695e466ae3378dfb2242fc
SondreB <sondre@outlook.com>
2022-02-02 20:08:58 +0100

    Add address formatting to dim the decimal
    - Related to #31

--- src/Blockcore.Explorer/ClientApp/src/app/shared/amount.ts
+++ src/Blockcore.Explorer/ClientApp/src/app/shared/amount.ts
@@ -12,3 +12,22 @@ export class AmountPipe implements PipeTransform {
       return this.setup.transformFormat(value);
    }
 }
+
+@Pipe({ name: 'amountHtml', pure: false })
+export class AmountMarkdownPipe implements PipeTransform {
+
+   constructor(public setup: SetupService) {
+
+   }
+
+   transform(value: number): string {
+      const amount = this.setup.transformFormat(value);
+
+      if (amount.indexOf('.') > -1) {
+         const amounts = amount.split('.');
+         return `${amounts[0]}.<span class="decimals">${amounts[1]}</span>`;
+      } else {
+         return amount;
+      }
+   }
+}
