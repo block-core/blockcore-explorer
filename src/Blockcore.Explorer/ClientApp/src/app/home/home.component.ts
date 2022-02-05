88ee79873ded1468382c1bdff80daa8f4d88b3c1
SondreB <sondre@outlook.com>
2022-02-05 14:26:59 +0100

    Add prices on home page and 24 hour change difference

--- src/Blockcore.Explorer/ClientApp/src/app/home/home.component.ts
+++ src/Blockcore.Explorer/ClientApp/src/app/home/home.component.ts
@@ -1,18 +1,51 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { SetupService } from '../services/setup.service';
 import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   // @HostBinding('class.content-centered') hostClass = true;
+  tickers: any;
 
-  constructor(public setup: SetupService, private router: Router) {
+  constructor(
+    public setup: SetupService,
+    private api: ApiService,
+    private router: Router) {
     // When we are not in multichain mode, redirect to chain-home.
     if (!setup.multiChain) {
       router.navigate(['/' + setup.current.toLowerCase()]);
+    }
+  }
+
+  async ngOnInit() {
+    await this.updateTicker();
+  }
+
+  getChangeClass(value: number) {
+    if (value < 0) {
+      return 'negative';
+    } else {
+      return 'positive';
+    }
+  }
+
+  async updateTicker() {
+
+    try {
+      const coins = this.setup.chains.map(c => c.coin).filter((c) => c != null);;
+      const coinList = coins.join('%2C');
+      const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coinList}&vs_currencies=btc&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`
+
+      const request = await this.api.download(url);
+      this.tickers = request;
+    }
+    catch (err) {
+      console.error(err);
     }
   }
 }
