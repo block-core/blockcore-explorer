3e644e6af81fefb0fd2eee78bc268015f9d89572
SondreB <sondre@outlook.com>
2022-03-14 21:53:47 +0100

    Hide errors if issues with the API results

--- src/Blockcore.Explorer/ClientApp/src/app/ticker/ticker.component.ts
+++ src/Blockcore.Explorer/ClientApp/src/app/ticker/ticker.component.ts
@@ -26,15 +26,20 @@ export class TickerComponent implements OnInit, OnDestroy {
   }
 
   navigator = (obj, path) => {
-    for (var i = 0, path = path.split('.'), len = path.length; i < len; i++) {
-      if (Array.isArray((obj[path[i]]))) {
-        obj = obj[path[i]][path[i + 1]];
-        i++;
-      } else {
-        obj = obj[path[i]];
-      }
-    };
-    return obj;
+    try {
+      for (var i = 0, path = path.split('.'), len = path.length; i < len; i++) {
+        if (Array.isArray((obj[path[i]]))) {
+          obj = obj[path[i]][path[i + 1]];
+          i++;
+        } else {
+          obj = obj[path[i]];
+        }
+      };
+      return obj;
+    }
+    catch (err) {
+      console.error(err);
+    }
   };
 
   async updateTicker() {
