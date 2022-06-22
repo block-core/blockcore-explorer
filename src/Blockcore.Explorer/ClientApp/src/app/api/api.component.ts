9c55686ab94941b5a21b87fb5f53fbbaa6f7258f
dangershony <dan.gershony@gmail.com>
2022-06-22 23:04:50 +0100

    first commit for selecting indexers

--- src/Blockcore.Explorer/ClientApp/src/app/api/api.component.ts
+++ src/Blockcore.Explorer/ClientApp/src/app/api/api.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, HostBinding } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { SetupService } from '../services/setup.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-api',
@@ -10,7 +11,45 @@ import { SetupService } from '../services/setup.service';
 export class ApiComponent {
   @HostBinding('class.content-centered-top') hostClass = true;
 
-  constructor(private route: ActivatedRoute, public setup: SetupService) {
+
+  endpoints: any;
+  dnsendpoints: any;
+
+  constructor(
+    private route: ActivatedRoute,
+     public setup: SetupService,
+     private api: ApiService,
+     ) {
     this.route.data.subscribe(data => console.log('Data :', data));
+
+ 
   }
+
+  async save() {
+    console.log('appi csave alled');
+  }
+
+
+  async discover() {
+    
+    if(this.dnsendpoints == null)
+    {
+      this.dnsendpoints =  await this.api.download('https://chains.blockcore.net/services/BLOCKCORE-DNS.json');
+    }
+  
+    console.log(this.dnsendpoints);
+
+
+    this.endpoints = [];
+    let chain = this.setup.current;
+
+    for (let index = 0; index < this.dnsendpoints.length; index++) {
+      const element = this.dnsendpoints[index];
+      let res = await this.api.download(element["dns-server"] + "/api/dns/services/symbol/"+ chain.toUpperCase() + "/service/Indexer");
+      console.log(res);
+    }
+
+  }
+
+
 }
