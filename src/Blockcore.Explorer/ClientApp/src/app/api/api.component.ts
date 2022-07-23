13b81cb3d090b420b271c88c18f15626084e4154
SondreB <sondre@outlook.com>
2022-07-23 22:54:42 +0200

    Use Blockcore npm packages for querying dynamic nameservers
    - Query the dynamic nameservers for online indexer services.

--- src/Blockcore.Explorer/ClientApp/src/app/api/api.component.ts
+++ src/Blockcore.Explorer/ClientApp/src/app/api/api.component.ts
@@ -1,91 +1,114 @@
-import { Component, Inject, HostBinding } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
-import { SetupService } from '../services/setup.service';
-import { ApiService } from '../services/api.service';
+import { Component, Inject, HostBinding } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
+import { SetupService } from "../services/setup.service";
+import { ApiService } from "../services/api.service";
+import { BlockcoreDns } from "@blockcore/dns";
+import { IndexerProvider } from "@blockcore/provider";
 
 @Component({
-  selector: 'app-api',
-  templateUrl: './api.component.html'
+   selector: "app-api",
+   templateUrl: "./api.component.html",
 })
 export class ApiComponent {
-  @HostBinding('class.content-centered-top') hostClass = true;
+   @HostBinding("class.content-centered-top") hostClass = true;
 
+   indexers: any;
+   dnsendpoints: any;
+   inputUrl: any;
+   docUrl: any;
+   errorMessage: any;
+   dns: BlockcoreDns;
+   indexerProvider: IndexerProvider;
 
-  endpoints: any;
-  dnsendpoints: any;
-  inputUrl: any;
-  docUrl: any;
-  errorMessage: any;
+   constructor(
+      private route: ActivatedRoute,
+      public setup: SetupService,
+      private api: ApiService
+   ) {
+      this.route.data.subscribe((data) => console.log("Data :", data));
 
-  constructor(
-    private route: ActivatedRoute,
-     public setup: SetupService,
-     private api: ApiService,
-     ) {
-    this.route.data.subscribe(data => console.log('Data :', data));
+      this.load();
+   }
 
-    this.load();
-  }
+   async load() {
+      this.inputUrl = await this.api.getBaseUrl();
 
-  async load(){
-    this.inputUrl = await this.api.getBaseUrl();
-
-    if(this.inputUrl == this.setup.Explorer.Indexer.ApiUrl){
-      this.docUrl =  this.setup.Explorer.Indexer.DocUrl;
-    }
-    else {
-      this.docUrl =  this.inputUrl.replace("api","docs");
-    }
-  }
-
-  async save() {
-    await this.api.setBaseUrl(this.setup.current, this.inputUrl );
-    await this.load();
-  }
-
-
-  async discover() {
-    
-    if(this.dnsendpoints == null)
-    {
-      this.dnsendpoints =  await this.api.download('https://chains.blockcore.net/services/BLOCKCORE-DNS.json');
-    }
-  
-    this.endpoints = [];
-    let chain = this.setup.current;
-
-    for (let index = 0; index < this.dnsendpoints.length; index++) {
-      const element = this.dnsendpoints[index];
-
-      try {
-        let res = await this.api.download(element["dns-server"] + "/api/dns/services/symbol/"+ chain.toUpperCase() + "/service/Indexer");
-        
-        res.Source = element["dns-server"];
-        res.Source = res.Source.replace("https://", "")
-        this.endpoints.push(res);
-     
-      } catch (err) {
-        if (err.message[0] === '{') {
-          this.errorMessage = JSON.parse(err.message);
-        } else {
-          this.errorMessage = err;
-        }
-        console.log(err);        
+      if (this.inputUrl == this.setup.Explorer.Indexer.ApiUrl) {
+         this.docUrl = this.setup.Explorer.Indexer.DocUrl;
+      } else {
+         this.docUrl = this.inputUrl.replace("api", "docs");
       }
-    }
-  }
+   }
 
-  async savedomain(event, item){
-    var domain = item.domain;
-    domain = "https://" + domain + "/api";   
-    this.inputUrl = domain;
-    await this.save();
-  }
+   async save() {
+      await this.api.setBaseUrl(this.setup.current, this.inputUrl);
+      await this.load();
+   }
 
-  async reset(){
-    await this.api.resetBaseUrl(this.setup.current);
-    await this.load();
-  }
+   async discover() {
+      // this.dnsendpoints = await BlockcoreDns.getDnsServers();
+      let chain = this.setup.current;
 
+      this.dns = new BlockcoreDns();
+      await this.dns.load();
+
+      this.indexers = this.dns.getOnlineServicesByNetwork(chain.toUpperCase());
+
+      // this.indexerProvider = new IndexerProvider();
+      // await this.indexerProvider.load();
+
+      // this.indexerProvider.setNetwork(chain.toUpperCase());
+
+      // // console.log(this.indexerProvider);
+      // // this.endpoints = 
+      // // let chain = this.setup.current;
+
+      // this.indexers = await this.indexerProvider..getIndexersByNetwork(
+      //    chain.toUpperCase()
+      // );
+
+      console.log(this.indexers);
+
+      // this.dns = new BlockcoreDns("");
+      // this.dns.setActiveServer();
+
+      // this.endpoints = [];
+
+      // for (let index = 0; index < this.dnsendpoints.length; index++) {
+      //    const element = this.dnsendpoints[index];
+
+      //    try {
+      //       let res = await this.api.download(
+      //          element["dns-server"] +
+      //             "/api/dns/services/symbol/" +
+      //             chain.toUpperCase() +
+      //             "/service/Indexer"
+      //       );
+
+      //       res.Source = element["dns-server"];
+      //       res.Source = res.Source.replace("https://", "");
+      //       this.endpoints.push(res);
+      //    } catch (err) {
+      //       if (err.message[0] === "{") {
+      //          this.errorMessage = JSON.parse(err.message);
+      //       } else {
+      //          this.errorMessage = err;
+      //       }
+      //       console.log(err);
+      //    }
+      // }
+   }
+
+   async saveDomain(event, item) {
+      var domain = item.domain;
+      domain = "https://" + domain + "/api";
+      this.inputUrl = domain;
+      await this.save();
+   }
+
+   async reset() {
+      await this.api.resetBaseUrl(this.setup.current);
+      await this.load();
+   }
 }
